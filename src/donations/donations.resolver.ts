42ecc650eae7cd6647e107018c62601216e3536e
hvlopez <hectorvmlopez@gmail.com>
2022-10-30 19:32:30 -0500

    adding validations to graphql schema, adding new types

--- src/donations/donations.resolver.ts
+++ src/donations/donations.resolver.ts
@@ -1,6 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { DonationCreateInput } from 'src/@generated/prisma-nestjs-graphql/donation/donation-create.input';
 import { DonationsService } from './donations.service';
-import { CreateDonationInput } from './dto/create-donation.input';
 
 @Resolver('Donation')
 export class DonationsResolver {
@@ -8,18 +8,24 @@ export class DonationsResolver {
 
   @Mutation('createDonation')
   create(
-    @Args('createDonationInput') createDonationInput: CreateDonationInput,
+    @Args('createDonationInput')
+    createDonationInput: DonationCreateInput,
   ) {
     return this.donationsService.create(createDonationInput);
   }
 
   @Query('donations')
-  findAll() {
-    return this.donationsService.findAll();
+  findAll(@Args('orderBy') orderBy?: { field?: string; direction?: string }) {
+    return this.donationsService.findAll(orderBy);
+  }
+
+  @Query('totalDonations')
+  totalDonations() {
+    return this.donationsService.getTotal();
   }
 
   @Query('donation')
   findOne(@Args('id') id: number) {
-    return this.donationsService.findOne(id);
+    return this.donationsService.findOne({ id });
   }
 }
--- src/donations/donations.service.ts
+++ src/donations/donations.service.ts
@@ -1,23 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { DonationCreateInput } from 'src/@generated/prisma-nestjs-graphql/donation/donation-create.input';
+import { DonationWhereUniqueInput } from 'src/@generated/prisma-nestjs-graphql/donation/donation-where-unique.input';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateDonationInput } from './dto/create-donation.input';
 
 @Injectable()
 export class DonationsService {
   constructor(private readonly prisma: PrismaService) {}
-  create(createDonationInput: CreateDonationInput) {
-    return 'This action adds a new donation';
+  create(createDonationInput: DonationCreateInput) {
+    return this.prisma.donation.create({ data: createDonationInput });
+  }
+  async getTotal() {
+    const res = await this.prisma.donation.aggregate({ _sum: { count: true } });
+    return res._sum.count;
+  }
+  findAll(orderBy?: { field?: string; direction?: string }) {
+    const { field = 'createdAt', direction = 'desc' } = orderBy || {};
+
+    return this.prisma.donation.findMany({ orderBy: { [field]: direction } });
   }
 
-  findAll() {
-    console.log(
-      'this.prisma.donation.findMany()',
-      this.prisma.donation.findMany(),
-    );
-    return this.prisma.donation.findMany();
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} donation`;
+  findOne(donationWhereUniqueInput: DonationWhereUniqueInput) {
+    return this.prisma.donation.findUnique({
+      where: { ...donationWhereUniqueInput },
+    });
   }
 }
